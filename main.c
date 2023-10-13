107721a9b3c03e00073be79dc69f43cfbd139f8e
^_^ <32826270+hcm444@users.noreply.github.com>
2023-10-13 16:54:00 -0700

    Update main.c
    optional lifts

--- main.c
+++ main.c
@@ -11,7 +11,7 @@ void calculateWeights(int week, int oneRepMax, float trainingPercentages[], cons
         {5, 5, 5},
         {3, 3, 3},
         {5, 3, 1},
-        {5, 5, 5}  
+        {5, 5, 5}
     };
 
     printf("Rep-Set Goals: %d x %d, %d x %d, %d x %d\n",
@@ -40,49 +40,64 @@ void calculateWeights(int week, int oneRepMax, float trainingPercentages[], cons
 }
 
 int main(int argc, char *argv[]) {
-    int squatMax = 300;
-    int benchMax = 250;
-    int ohpMax = 150;
-    int deadliftMax = 400;
+    int squatMax = 0;
+    int benchMax = 0;
+    int ohpMax = 0;
+    int deadliftMax = 0;
     int weekToDisplay = 0;
 
     for (int i = 1; i < argc; i++) {
         if (argv[i][0] == '-') {
             char option = argv[i][1];
-            switch (option) {
-                case 's':
-                    squatMax = atoi(argv[i + 1]);
-                    break;
-                case 'b':
-                    benchMax = atoi(argv[i + 1]);
-                    break;
-                case 'o':
-                    ohpMax = atoi(argv[i + 1]);
-                    break;
-                case 'd':
-                    deadliftMax = atoi(argv[i + 1]);
-                    break;
-                case 'w':
-                    weekToDisplay = atoi(argv[i + 1]);
-                    break;
-                default:
-                    fprintf(stderr, "Unknown option: -%c\n", option);
-                    return 1;
+            if (i + 1 < argc) {
+                switch (option) {
+                    case 's':
+                        squatMax = atoi(argv[i + 1]);
+                        break;
+                    case 'b':
+                        benchMax = atoi(argv[i + 1]);
+                        break;
+                    case 'o':
+                        ohpMax = atoi(argv[i + 1]);
+                        break;
+                    case 'd':
+                        deadliftMax = atoi(argv[i + 1]);
+                        break;
+                    case 'w':
+                        weekToDisplay = atoi(argv[i + 1]);
+                        if (weekToDisplay < 1 || weekToDisplay > NUM_WEEKS) {
+                            fprintf(stderr, "Invalid week number. Please select a week from 1 to %d.\n", NUM_WEEKS);
+                            return 1;
+                        }
+                        break;
+                    default:
+                        fprintf(stderr, "Unknown option: -%c\n", option);
+                        return 1;
+                }
             }
         }
     }
 
-    if (argc > 1) {
-        if (weekToDisplay >= 1 && weekToDisplay <= NUM_WEEKS) {
+    // Calculate routines only if at least one lift is specified.
+    if (squatMax > 0 || benchMax > 0 || ohpMax > 0 || deadliftMax > 0) {
+        if (argc > 1 && weekToDisplay == 0) {
+            for (int week = 1; week <= NUM_WEEKS; week++) {
+                float trainingPercentages[NUM_SETS];
+                if (squatMax > 0) calculateWeights(week, squatMax, trainingPercentages, "Squat");
+                if (benchMax > 0) calculateWeights(week, benchMax, trainingPercentages, "Bench Press");
+                if (ohpMax > 0) calculateWeights(week, ohpMax, trainingPercentages, "Overhead Press");
+                if (deadliftMax > 0) calculateWeights(week, deadliftMax, trainingPercentages, "Deadlift");
+            }
+        } else if (weekToDisplay > 0) {
             float trainingPercentages[NUM_SETS];
-            calculateWeights(weekToDisplay, squatMax, trainingPercentages, "Squat");
-            calculateWeights(weekToDisplay, benchMax, trainingPercentages, "Bench Press");
-            calculateWeights(weekToDisplay, ohpMax, trainingPercentages, "Overhead Press");
-            calculateWeights(weekToDisplay, deadliftMax, trainingPercentages, "Deadlift");
-        } else {
-            fprintf(stderr, "Invalid week number. Please select a week from 1 to %d.\n", NUM_WEEKS);
-            return 1;
+            if (squatMax > 0) calculateWeights(weekToDisplay, squatMax, trainingPercentages, "Squat");
+            if (benchMax > 0) calculateWeights(weekToDisplay, benchMax, trainingPercentages, "Bench Press");
+            if (ohpMax > 0) calculateWeights(weekToDisplay, ohpMax, trainingPercentages, "Overhead Press");
+            if (deadliftMax > 0) calculateWeights(weekToDisplay, deadliftMax, trainingPercentages, "Deadlift");
         }
+    } else {
+        fprintf(stderr, "At least one lift (squat, bench, ohp, deadlift) must be specified.\n");
+        return 1;
     }
 
     return 0;
